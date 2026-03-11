b4dcf7603f0c583f1a29dd4a5b6bb94b501729cf
Gaurav-Yadav04 <gaurav4004ayd@gmail.com.>
2026-03-11 10:35:01 +0530

    add signIn logic

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import {ClerkProvider} from '@clerk/react'
+
+const PUBLISHABKE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if(!PUBLISHABKE_KEY){
+  throw new Error('Missing Publishable Key!')
+}
 
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABKE_KEY}>
+   <BrowserRouter>
+     <App />
+   </BrowserRouter>
+  </ClerkProvider>,
+ 
 )
